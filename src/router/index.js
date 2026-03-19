7359285b7d7bb4c21df6a8ca8bbd400fe44c9233
Nguyen Le Minh Duy <duyst24v7x606@dttx.ctu.edu.vn>
2026-03-19 19:55:55 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
-import { createWebHistory, createRouter } from "vue-router"; 
-import ContactBook from "@/views/ContactBook.vue";
+import { createWebHistory, createRouter } from "vue-router";
+import ContactBook from "../views/ContactBook.vue";
+
 const routes = [
-{
-path: "/",
-name: "ContactBook", 
-component: ContactBook,
-},
+    {
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("../views/NotFound.vue"),
+    },
 ];
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL), routes,
